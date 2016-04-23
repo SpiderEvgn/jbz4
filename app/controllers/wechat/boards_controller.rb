d28ae4834c6d147465116988a69261ba73bcc97e
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-04-23 17:19:34 +0800

    move getJbzforetellInfo & getJbzhotfilmInfo to boards_controller, and Jbzhotfilm relies on Jbzforetell

--- app/controllers/wechat/boards_controller.rb
+++ app/controllers/wechat/boards_controller.rb
@@ -1,6 +1,13 @@
 class Wechat::BoardsController < ApplicationController
   layout 'wechat'
 
+  # before_action :getJbzforetellInfo
+  # 这个 action 用来从 maizuoforetells 获取最近三天的排期数据并存入 jbzforetells 表
+  # before_action :getJbzhotfilmInfo
+  # 这个 action 用来自动获取最近三天热映的电影信息并存入 jbzhotfilms 表
+  # 还没想明白怎么很好地从api拉取数据并导入本地数据库，暂且临时激活一个 action 将数据一次性导入，
+  # 然后就注释掉不用了，之后 index 数据就直接从本地数据库读
+
   def jbz_hotfilm
     @films = Jbzhotfilm.all
   end
@@ -14,5 +21,85 @@ class Wechat::BoardsController < ApplicationController
   	@cinemas = Maizuocinema.all
   	# 还没建立 Jbzcinema, 暂用 Maizuocinema
   end
+
+  private 
+    # 考虑将以下自动获取热映电影代码放入 jbzhotfilm 的 model 里面去，下次 commit
+    d1 = Time.new.strftime("%d")     # 当日的天数
+    d2 = (d1.to_i + 1).to_s          # 明日的天数
+    d3 = (d2.to_i + 1).to_s          # 后日的天数
+    D1 = String.new("2016-04-#{d1}") # 当天的日期
+    D2 = String.new("2016-04-#{d2}") # 明天的日期
+    D3 = String.new("2016-04-#{d3}") # 后天的日期（查最近三天的热映电影）
+
+    def getJbzforetellInfo
+      # Jbzforetell.destroy_all
+      @maizuoforetells = Maizuoforetell.where("showDate = ? OR showDate = ? OR showDate = ?", "#{D1}", "#{D2}", "#{D3}").all
+      # 先通过今、明、后三天的日期筛选出排期表中所有在最近三天排期数据，存入到本地 jbzforetell 排期
+      @maizuoforetells.each do |mzft|
+        f = Jbzforetell.new
+
+        f.cinemaId = mzft.cinemaId
+        f.showDate = mzft.showDate
+        f.showTime = mzft.showTime
+        f.hallId = mzft.hallId
+        f.hallName = mzft.hallName
+        f.foretellId = mzft.foretellId
+        f.filmId = mzft.filmId
+        f.price = mzft.price
+        f.marketPrice = mzft.marketPrice
+        f.flag = mzft.flag
+        f.language = mzft.language
+        f.duration = mzft.duration
+        f.timeout = mzft.timeout
+        f.sectionId = mzft.sectionId
+        f.dimensional = mzft.dimensional
+        f.offerId = mzft.offerId
+        f.lockNeedMobile = mzft.lockNeedMobile
+
+        f.save
+      # @maizuoforetells.each do |mzft|
+      end
+    # def getJbzforetellInfo
+    end
+    
+    def getJbzhotfilmInfo
+      # Jbzhotfilm.destroy_all
+      @jbzhotfilmIds = Jbzforetell.select(:filmId).uniq
+      # 从已有的 Jbzforetells 表中取出所有在最近三天热映的电影，然后对 filmId 取 uniq 值，得到一个数组
+      @jbzhotfilmIds.each do |mzfid|
+        jbzfid = mzfid.filmId   # 将数组中的 filmId 值取出，用作以下到电影库中比对并存入 jbzhotfilm 表
+        @maizuofilm = Maizuofilm.find_by_filmId("#{jbzfid}")
+        f = Jbzhotfilm.new
+
+        f.filmId = jbzfid
+        f.name = @maizuofilm.name
+        f.director = @maizuofilm.director
+        f.actors = @maizuofilm.actors
+        f.filmType = @maizuofilm.filmType
+        f.area = @maizuofilm.area
+        f.length = @maizuofilm.length
+        f.releaseDate = @maizuofilm.releaseDate
+        f.isShow = @maizuofilm.isShow
+        f.language = @maizuofilm.language
+        f.company = @maizuofilm.company
+        f.info = @maizuofilm.info
+        f.picAddr = @maizuofilm.picAddr
+        f.dimensional = @maizuofilm.dimensional
+        f.shortInfo = @maizuofilm.shortInfo
+        f.videoUrl = @maizuofilm.videoUrl
+        f.grade = @maizuofilm.grade
+
+        f.save
+      # @jbzfilmIds.each do |fi|
+      end
+    # def getJbzfilmInfo
+    end
+
+    # strong_params 没有用到，暂且保留
+    # def jbzhotfilm_params
+    #   params.require(:jbzhotfilm).permit(:name, :director, :actors, :filmType,
+    #                                      :area, :length, :releaseDate, :dimensional,
+    #                                      :shortInfo)
+    # end
 
 end
--- app/controllers/wechat/jbzlocal/jbzhotfilms_controller.rb
+++ app/controllers/wechat/jbzlocal/jbzhotfilms_controller.rb
@@ -1,61 +1,9 @@
 class Wechat::Jbzlocal::JbzhotfilmsController < ApplicationController
   layout 'wechat'
-  # before_action :getJbzhotfilmInfo
-  # 这个 action 用来自动获取最近三天热映的电影信息并存入 jbzhotfilms 表
-  # 还没想明白怎么很好地从api拉取数据并导入本地数据库，暂且临时激活一个 action 将数据一次性导入，
-  # 然后就注释掉不用了，之后 index 数据就直接从本地数据库读
-  
+
   def show
   	# 在热映电影列表点击一部电影后，把该电影ID传过来，启用 show 来显示该电影完整信息
     @jbzhotfilm = Jbzhotfilm.find_by_filmId(params[:id])
   end
   
-  private 
-    # 考虑将以下自动获取热映电影代码放入 jbzhotfilm 的 model 里面去，下次 commit
-    d1 = Time.new.strftime("%d")     # 当日的天数
-    d2 = (d1.to_i + 1).to_s          # 明日的天数
-    d3 = (d2.to_i + 1).to_s          # 后日的天数
-    D1 = String.new("2016-04-#{d1}") # 当天的日期
-    D2 = String.new("2016-04-#{d2}") # 明天的日期
-    D3 = String.new("2016-04-#{d3}") # 后天的日期（查最近三天的热映电影）
-
-    def getJbzhotfilmInfo
-    	Jbzhotfilm.destroy_all
-      @jbzfilmIds = Maizuoforetell.where("showDate = ? OR showDate = ? OR showDate = ?", "#{D1}", "#{D2}", "#{D3}").select(:filmId).uniq
-      # 先通过今、明、后三天的日期筛选出排期表中所有在最近三天热映的电影，然后对 filmId 取 uniq 值，得到一个 hash
-      @jbzfilmIds.each do |mzfid|
-        jbzfid = mzfid.filmId   # 将hash数组中的 filmId 值取出，用作以下到电影库中比对并存入 jbzhotfilm 表
-        @maizuofilm = Maizuofilm.find_by_filmId("#{jbzfid}")
-        f = Jbzhotfilm.new
-
-        f.filmId = jbzfid
-        f.name = @maizuofilm.name
-        f.director = @maizuofilm.director
-        f.actors = @maizuofilm.actors
-        f.filmType = @maizuofilm.filmType
-        f.area = @maizuofilm.area
-        f.length = @maizuofilm.length
-        f.releaseDate = @maizuofilm.releaseDate
-        f.isShow = @maizuofilm.isShow
-        f.language = @maizuofilm.language
-        f.company = @maizuofilm.company
-        f.info = @maizuofilm.info
-        f.picAddr = @maizuofilm.picAddr
-        f.dimensional = @maizuofilm.dimensional
-        f.shortInfo = @maizuofilm.shortInfo
-        f.videoUrl = @maizuofilm.videoUrl
-        f.grade = @maizuofilm.grade
-
-        f.save
-      # @jbzfilmIds.each do |fi|
-      end
-    # def getJbzfilmInfo
-    end
-
-    # strong_params 没有用到，暂且保留
-    # def jbzhotfilm_params
-    #   params.require(:jbzhotfilm).permit(:name, :director, :actors, :filmType,
-    #                                      :area, :length, :releaseDate, :dimensional,
-    #                                      :shortInfo)
-    # end
 end
