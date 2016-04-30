af7c1d16df9ef78e292966f3758cfaf68bb11281
SpiderEvgn <xiajun.zhang@usingnow.com>
2016-04-30 17:42:48 +0800

    add realtimeseats/show, now you can go to realtimeseats through Buying Ticket button

--- app/controllers/wechat/jbzlocal/realtimeseats_controller.rb
+++ app/controllers/wechat/jbzlocal/realtimeseats_controller.rb
@@ -2,12 +2,8 @@ class Wechat::Jbzlocal::RealtimeseatsController < ApplicationController
   layout 'wechat'
 
   def show
-  	# 把 foretellId 传进来，调用 realTimeSeat 接口获取实时座位 JSON
-
-  end
-
-  def index
-  	@seatresource = Wechat::Maizuo::Realtimeseat.getRealTimeSeats("1053015060").to_json
+    # 把 foretellId 传进来，调用 realTimeSeat 接口获取实时座位 JSON
+    @seatresource = Wechat::Maizuo::Realtimeseat.getRealTimeSeats(params[:id]).to_json
   end
 
 end
